db41fbdbde84a363fc10e0f33345a31d7a743110
Aleksandr Bykanov <aleksandrbykanov1986@gmail.com>
2025-04-21 18:35:24 +0300

    header

---
+++ src/js/main.js
@@ -0,0 +1,72 @@
+import '../scss/style.scss';
+import '../scss/style.scss';
+
+const dropdownButton = document.querySelector('.header-language-btn');
+const dropdownContent = document.querySelector('.header-language-list');
+const selectedLanguage = document.querySelector('.selected-language');
+const arrowSVG = document.querySelector(".header-language-btn svg use"); 
+
+
+dropdownButton.addEventListener('click', (event) => {
+  event.stopPropagation(); 
+  const isMenuOpen = dropdownContent.style.display === 'block';
+  arrowSVG.setAttribute(
+    "href",
+    isMenuOpen ? "./public/svg/icons.svg#icon-arrow-down" : "./public/svg/icons.svg#icon-arrow-up"
+  );
+  isMenuOpen ? selectedLanguage.style.color = '#919192' : selectedLanguage.style.color = '#e9e9e9';
+  dropdownContent.style.display = isMenuOpen ? 'none' : 'block';
+});
+
+
+dropdownContent.addEventListener('click', (event) => {
+  event.stopPropagation(); 
+  const li = event.target.closest('li');
+  if (li) {
+    const languageCode = li.getAttribute('data-lang');
+    selectedLanguage.textContent = languageCode;
+    dropdownContent.style.display = 'none';
+    selectedLanguage.style.color = '#919192';
+    arrowSVG.setAttribute("href", "./public/svg/icons.svg#icon-arrow-down");
+  }
+});
+
+
+document.addEventListener('click', () => {
+  dropdownContent.style.display = 'none';
+  arrowSVG.setAttribute("href", "./public/svg/icons.svg#icon-arrow-down");
+  selectedLanguage.style.color = '#919192'; 
+});
+
+
+const burgerInput = document.querySelector('.burger-input');
+const modal = document.querySelector('.modal');
+const menuItems = document.querySelectorAll('.modal-item');
+
+function checkScreenSize() {
+  if (window.innerWidth >= 1200 && modal.classList.contains('active')) {
+    modal.classList.remove('active');
+    burgerInput.checked = false;
+  }
+}
+
+burgerInput.addEventListener('change', () => {
+  if (burgerInput.checked) {
+    modal.classList.add('active');
+    header.classList.add('transparent');
+  } else {
+    modal.classList.remove('active');
+  }
+});
+
+
+menuItems.forEach(item => {
+  item.addEventListener('click', () => {
+    modal.classList.remove('active');
+    header.classList.remove('transparent');
+    burgerInput.checked = false; 
+  });
+});
+
+
+
